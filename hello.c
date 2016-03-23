a3b2aa5b144582ff2bac9ed06c4a41a5cb10e1b0
Dylan Ferguson <Dylan@df-mbp.local>
2016-03-23 13:35:42 -0400

    Added Comments

--- hello.c
+++ hello.c
@@ -2,11 +2,18 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 
+//this function runs on load of the module, treat this as you would main().
 int init_module(void)
 {
+    /*printk prints to a kernel log, the KERN_INFO section
+     can be replaced with diffrent logging levels. We use this
+     since we are in kernel space not user space and unable to call printf
+    */
     printk(KERN_INFO "HELLO:: This is our LKM speaking! \n");
     return 0;
 }
+
+//this function runs on close of module
 void cleanup_module(void)
 {
     printk(KERN_INFO "HELLO:: This is our LKM leaving :( \n");
